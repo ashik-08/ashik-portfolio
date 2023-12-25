488b949b5113d5b86662e5fe54b0554b8c135620
ashik-08 <ashikurrahman.8october@gmail.com>
2023-12-25 06:10:27 +0600

    added service logo and links on header

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,47 @@
+import Link from "next/link";
+// icons
+import {
+  RiInstagramLine,
+  RiFacebookLine,
+  RiLinkedinLine,
+  RiTwitterLine,
+  RiGithubLine,
+} from "react-icons/ri";
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-2xl">
+      <Link
+        href={"https://www.facebook.com/ashikurrahman.ashik.568632"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiFacebookLine />
+      </Link>
+      <Link
+        href={"https://www.instagram.com/a_s_h_i_k_8/"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiInstagramLine />
+      </Link>
+      <Link
+        href={"https://twitter.com/ashikurrahman81"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiTwitterLine />
+      </Link>
+      <Link
+        href={"https://www.linkedin.com/in/md-ashikur-rahman-ashik-1b51231a3"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiLinkedinLine />
+      </Link>
+      <Link
+        href={"https://github.com/ashik-08"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiGithubLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
