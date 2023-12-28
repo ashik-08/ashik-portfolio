4197105584a4279bb80adeadba5a60562fe5d7d3
ashik-08 <ashikurrahman.8october@gmail.com>
2023-12-28 11:08:27 +0600

    added about info

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
           {/* logo */}
-          <Link href={'/'}>
+          <Link href={"/"}>
             {/* <Image src={'/logo.svg'} width={220} height={48} alt="" priority={true} /> */}
             <h3 className="text-3xl font-bold text-center">ashikur rahman</h3>
           </Link>
